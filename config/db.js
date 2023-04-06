a7ed3225d306b535b17c3e4488bb690956486f6c
Jonathan Ato Markin <jona10711@gmail.com>
2023-04-06 16:05:43 +0200

    Project structure created

---
+++ config/db.js
@@ -0,0 +1,11 @@
+//Connect to MongoDB
+const mongoose = require("mongoose");
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
