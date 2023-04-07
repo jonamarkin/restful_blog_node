891f03173311bc21813af54967491a8c3726b92a
Jonathan Ato Markin <jona10711@gmail.com>
2023-04-07 03:02:36 +0200

    Applied auth to user endpoints

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,19 +4,20 @@ const {
   getAllUsers,
   updateUser,
 } = require("../controllers/userController");
+const authMiddleware = require("../middlewares/authMiddleware");
 const userRouter = express.Router();
 
 //Get user by id
-userRouter.get("/:id", getUserById);
+userRouter.get("/:id", authMiddleware, getUserById);
 
 //Get all users
-userRouter.get("/", getAllUsers);
+userRouter.get("/", authMiddleware, getAllUsers);
 
 //Update user
-userRouter.put("/update/:id", updateUser);
+userRouter.put("/update/:id", authMiddleware, updateUser);
 
 //Update user's own profile
-userRouter.put("/update", updateUser);
+userRouter.put("/update", authMiddleware, updateUser);
 
 //Export router
 module.exports = userRouter;
