24865a47537a6efebe49c2bb87d4a8dabe18b613
Jonathan Ato Markin <jona10711@gmail.com>
2023-04-07 03:59:28 +0200

    Post routes added to index

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("./config/db");
 //Import routes
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
+const postRouter = require("./routes/postRoutes");
 
 //Define global variables and constants
 const PORT = process.env.PORT || 3000;
@@ -23,6 +24,7 @@ app.use(express.json({ extended: false }));
 //Define routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
 
 //Error handler
 app.use(globalErrorHandler);
